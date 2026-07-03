1e942eb043560127a161218a0b24bafac388fc4d
Giacomo <jacklugano@gmail.com>
2026-07-03 13:26:16 +0200

    feat: initialize Firebase SDK configuration and hosting cache

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,13 @@ import { getFirestore } from 'firebase/firestore';
 // Incolla qui le chiavi reali ottenute dalle Impostazioni Progetto della Console Firebase.
 // Per default usa il projectId fornito 'ermal-mondial-game'.
 const firebaseConfig = {
-  apiKey: "AIzaSyDummyKeyReplaceThisWithRealKey",
+  apiKey: "AIzaSyCn7wpcsgiKJue7-wuZKr_rQ8lqSuLrG7Q",
   authDomain: "ermal-mondial-game.firebaseapp.com",
   projectId: "ermal-mondial-game",
-  storageBucket: "ermal-mondial-game.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:1234567890abcd"
+  storageBucket: "ermal-mondial-game.firebasestorage.app",
+  messagingSenderId: "746215979041",
+  appId: "1:746215979041:web:c9ce7570e22b8c6ef2cb60",
+  measurementId: "G-273F2S92S3"
 };
 
 const app = initializeApp(firebaseConfig);
